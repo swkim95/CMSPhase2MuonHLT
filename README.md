9c0ffda4ea0d2a4d0c995fdbe6259cc5537da256
Minseok Oh <minseok.oh@cern.ch>
2020-09-25 12:42:03 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# CMS Phase-2 Muon HLT
+
+```
+cmsrel CMSSW_11_1_3
+cd CMSSW_11_1_3/src
+cmsenv
+git cms-init
+
+# L1TkMuon Filter
+git cms-addpkg DataFormats/HLTReco
+git cms-addpkg HLTrigger/HLTcore
+git cms-addpkg HLTrigger/HLTfilters
+git remote add khaosmos93 https://github.com/khaosmos93/cmssw.git
+git fetch khaosmos93
+git cherry-pick c9f4616d164b7689e2f87eb6ffa33f844e41d910
+git cherry-pick 4180226821c4e16ca6203efd38c3ca79936e1831
+
+# Bug fix for L1TkMuon
+git cms-merge-topic 31342
+git cms-checkdeps -a
+
+scram b -j 8
+```
+
---
+++ cfgs/harvest.py
@@ -0,0 +1,85 @@
+
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Phase2C9_cff import Phase2C9
+
+process = cms.Process('HARVESTING',Phase2C9)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2026D49Reco_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.DQMSaverAtRunEnd_cff')
+process.load('Configuration.StandardSequences.Harvesting_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32( -1 ),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# Input source
+process.source = cms.Source("DQMRootSource",
+    fileNames = cms.untracked.vstring( "file:DQMIO_IOFromL1TkMuon_L1TkMu22_dEta035_dPhi02.root" )
+)
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring('ProductNotFound'),
+    SkipEvent = cms.untracked.vstring(),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(1)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step4 nevts:100'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T15', '')
+
+# # FastTimerService client
+process.load('HLTrigger.Timer.fastTimerServiceClient_cfi')
+process.fastTimerServiceClient.dqmPath = "HLT/TimerService"
+
+process.timingPath = cms.Path( process.fastTimerServiceClient )
+
+from HLTriggerOffline.Common.HLTValidationHarvest_cff import *
+process.trackValPath = cms.Path( process.postProcessorMuonTrackHLT )
+process.dqmsave_step = cms.Path(process.DQMSaver)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.timingPath, process.trackValPath, process.dqmsave_step)
+
+
